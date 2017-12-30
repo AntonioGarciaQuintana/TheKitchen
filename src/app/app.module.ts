ceb96440ff0d65e6562f88540651ce5fd92c6d5d
Antonio Garcia <franciscoantoniogarciaquintana@gmail.com>
2017-12-29 23:16:33 -0700

    form de catalogo de restaurantes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RestaurantManagementComponent } from './catalogos/restaurantes/restaurantManagement.component';
+import { HomeComponent } from './home/home.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'restaurantManagement', component: RestaurantManagementComponent}
+];
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RestaurantManagementComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/catalogos/restaurantes/restaurantManagement.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-restaurant-management',
+    templateUrl: './restaurantManagement.component.html'
+})
+export class RestaurantManagementComponent {
+
+    titulo: String = 'asi nomas quedo';
+}
